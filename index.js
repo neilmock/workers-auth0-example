6fd6fdfbca34a49a8002c0a4e7f1ccc012915497
Kristian Freeman <kristian@kristianfreeman.com>
2020-04-01 13:13:22 -0500

    move to workers sites for originless response
    workers sites is generally a better solution for devs to explore using this project in an originless deployment. this commit moves the project to workers sites, and using alpine.js + htmlrewriter, shows how to easily inline data from kv into static html pages. #jamstackontheedge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { authorize, logout, handleRedirect } from './auth0'
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
-import index from './templates/index'
-import logged_out from './templates/logged_out'
+import { authorize, logout, handleRedirect } from './auth0'
+import { hydrateState } from './edge_state'
 
 addEventListener('fetch', event => event.respondWith(handleRequest(event)))
 
@@ -12,6 +12,7 @@ const config = {
 async function handleRequest(event) {
   try {
     let request = event.request
+
     const [authorized, { authorization, redirectUrl }] = await authorize(event)
     if (authorized && authorization.accessToken) {
       request = new Request(request, {
@@ -26,17 +27,6 @@ async function handleRequest(event) {
       : await fetch(event.request)
 
     const url = new URL(event.request.url)
-    if (url.pathname === '/logout') {
-      const { headers } = logout(event)
-      return headers
-        ? new Response(logged_out(), {
-            headers: Object.assign({}, headers, {
-              'Content-type': 'text/html',
-            }),
-          })
-        : Response.redirect(url.origin)
-    }
-
     if (url.pathname === '/auth') {
       const authorizedResponse = await handleRedirect(event)
       response = new Response(response.body, {
@@ -50,11 +40,29 @@ async function handleRequest(event) {
       return Response.redirect(redirectUrl)
     }
 
-    return config.originless
-      ? new Response(index({ userInfo: authorization.userInfo }), {
-          headers: { 'Content-type': 'text/html' },
-        })
-      : response
+    response = await getAssetFromKV(event)
+
+    if (url.pathname === '/logout') {
+      const { headers } = logout(event)
+      return headers
+        ? new Response(response.body, {
+            ...response,
+            headers: Object.assign({}, response.headers, headers),
+          })
+        : Response.redirect(url.origin)
+    }
+
+    // hydrate the static site with authorization info from auth0
+    // this uses alpine.js and the htmlrewriter engine built into
+    // workers. for more info, check out the README
+    return new HTMLRewriter()
+      .on('script#edge_state', hydrateState(authorization.userInfo))
+      .transform(response)
+
+    // to skip any sort of edge-state hydration (see the README) and just
+    // return your site, replace the above return call with the line below
+    //
+    // return response
   } catch (err) {
     return new Response(err.toString())
   }
